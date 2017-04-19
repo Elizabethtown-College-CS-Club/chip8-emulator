a80600918622adecc80dce35f2d07d8ec6b99de7
Stottler <JAYNET\stottlerb@e281-000-linux.etown.edu>
2017-04-19 19:53:18 -0400

    Implement more opcodes

--- src/Chip8.cpp
+++ src/Chip8.cpp
@@ -14,8 +14,8 @@ Chip8::Chip8(const char gameFile[])
 
 	fclose(file);
 
-	for (int i = 0; i < sizeof(memory); i += 2)
-		printf("(%d) %04X ", i, memory[i] << 8 | memory[i+1]);
+	//for (int i = 0; i < sizeof(memory); i += 2)
+	//	printf("(%d) %04X ", i, memory[i] << 8 | memory[i+1]);
 }
 
 void Chip8::reset()
@@ -50,6 +50,19 @@ void Chip8::tick()
 	int pcDelta = 1;
 
 	switch (opcode >> 12) {
+		case 0x0: {
+			if ((opcode & 0x000F) == 0x0) { // 00E0
+				for (int i=0; i<64; i++)
+					for (int j=0; j<32; j++)
+						pixels[i][j] = false;
+				printf("%04X (00E0): clearing the screen\n", opcode);
+			} else { // 00EE
+				stackPointer--; // make sure this is != 0
+				programCounter = stack[stackPointer];
+
+				printf("%04X (00EE): returning to address %03X\n", opcode, programCounter);
+			}
+		} break;
 		case 0x1: { // 1NNN: Jump to address
 			programCounter = opcode & 0x0FFF;
 			pcDelta = 0;
@@ -94,6 +107,79 @@ void Chip8::tick()
 			registers[reg] = opcode & 0x00FF;
 
 			printf("%04X (6XNN): Placed value %d in register %X\n", opcode, opcode & 0xFF, reg);
+		} break;
+		case 0x7: { // 7XNN
+			int reg = (opcode & 0x0F00) >> 8;
+			registers[reg] += opcode & 0x00FF;
+
+			printf("%04X (7XNN): Added value %d to register %X\n", opcode, opcode & 0xFF, reg);
+		} break;
+		case 0x8: {
+			int regX = (opcode & 0x0F00) >> 8;
+			int regY = (opcode & 0x00F0) >> 4;
+			int value;
+			
+			switch(opcode & 0x000F)
+			{
+				case 0x0:
+					registers[regX] = registers[regY];
+					printf("%04X (8XY0): V[%X] = V[%X]\n", opcode, regX, regY);
+					break;
+				case 0x1:
+					registers[regX] |= registers[regY];
+					registers[0xF] = 0;
+					printf("%04X (8XY1): V[%X] |= V[%X]\n", opcode, regX, regY);
+					break;
+				case 0x2:
+					registers[regX] &= registers[regY];
+					registers[0xF] = 0;
+					printf("%04X (8XY2): V[%X] &= V[%X]\n", opcode, regX, regY);
+					break;
+				case 0x3:
+					registers[regX] ^= registers[regY];
+					registers[0xF] = 0;
+					printf("%04X (8XY3): V[%X] ^= V[%X]\n", opcode, regX, regY);
+					break;				
+				case 0x4:
+					value = (int)registers[regX] + (int)registers[regY];
+					if (value != (unsigned char)value)
+						registers[0xF] = 1; // Overflow occurs
+					else
+						registers[0xF] = 0;
+					registers[regX] = value;
+					printf("%04X (8XY4): V[%X] += V[%X]\n", opcode, regX, regY);
+					break;
+				case 0x5:
+					if (registers[regY] > registers[regX])
+						registers[0xF] = 0; // Borrow occurs
+					else
+						registers[0xF] = 1;
+					registers[regX] -= registers[regY];
+					printf("%04X (8XY5): V[%X] -= V[%X]\n", opcode, regX, regY);
+					break;
+				case 0x6:
+					value = registers[regX] & 1;
+					registers[regX] >>= 1;
+					registers[0xF] = value;
+					printf("%04X (8XY6): V[%X] >>= 1\n", opcode, regX);
+					break;
+				case 0x7:
+					if (registers[regX] > registers[regY])
+						registers[0xF] = 0; // Borrow occurs
+					else
+						registers[0xF] = 1;
+					registers[regX] = registers[regY] - registers[regX];
+					printf("%04X (8XY7): V[%X] = V[%X] - V[%X]\n", opcode, regX, regX, regY);
+					break;
+				case 0xE:
+					value = (registers[regX] >> 7) & 1;
+					registers[regX] <<= 1;
+					registers[0xF] = value;
+					printf("%04X (8XYE): V[%X] <<= 1\n", opcode, regX);
+					break;
+				default:
+					break;
+			} 
 		} break;
 		case 0xA: { // ANNN
 			address = opcode & 0x0FFF;
